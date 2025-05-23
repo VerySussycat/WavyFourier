bb54d532c5582b67e1134a9ef25a0e5487b7ec85
VerySussycat <108729015+VerySussycat@users.noreply.github.com>
2025-05-23 19:11:27 +0530

    Update SHM.py

--- SHM.py
+++ SHM.py
@@ -2,4 +2,4 @@ from manim import * #Import everything from manim
 class Example(Scene): #Kuch hai
     def construct(self):
         cube=Square(color=GREEN,fill_opacity=0.5)
-        self.add(cube)
+        
