42fbff2fe9f29d7622d66a60e2b3773080fd38ae
VerySussycat <shilpisid14@gmail.com>
2025-05-24 22:11:57 +0530

    Start

--- SHM.py
+++ SHM.py
@@ -1,5 +1,6 @@
 from manim import * #Import everything from manim
-class Example(Scene): #Kuch hai
+class Blockonspring(Scene): #Kuch hai
     def construct(self):
         cube=Square(color=GREEN,fill_opacity=0.5)
+        self.add(cube)
         
