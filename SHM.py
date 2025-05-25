4a3dda21ccbedad7caf9a558a6538a3acbe8065c
VerySussycat <108729015+VerySussycat@users.noreply.github.com>
2025-05-25 10:39:36 +0530

    Update SHM.py
    added spring

--- SHM.py
+++ SHM.py
@@ -9,7 +9,9 @@ class Blockonspring(Scene):#Kuch hai
         A=2     #Amplitude(initial displacement from equilibrium in this case as v_init=0)
         phi=0   #phase constant
         omega=(k/m)**0.5
-        cube=Square(color=DARK_BLUE,fill_opacity=0.5).move_to([1,2,0],aligned_edge=LEFT)   #initialize block
+        cube=Square(color=DARK_BLUE,fill_opacity=0.5).move_to([-3,-2,0],aligned_edge=LEFT).scale(0.5) #initialize block
+        wall=Line([-6,-2.5,0],[-6,4,0])
+        floor=Line([-6,-2.5,0],[6,-2.5,0])
         t=ValueTracker(0)
         def move(mob,dt):
             global vel
@@ -17,6 +19,17 @@ class Blockonspring(Scene):#Kuch hai
             acc=-(omega**2)*xfromq
             vel+=acc*dt
             mob.shift(vel*dt*RIGHT)
+        def zigzag(t):
+            l=np.modf(30*(t/(cube.get_critical_point(LEFT)[0]+6)))[1]+1
+            m=np.modf(30*(t/(cube.get_critical_point(LEFT)[0]+6)))[0]
+            if l%2==0:
+                return [t-6,0.4*m-2.5,0]
+            else:
+                return [t-6,0.4*(1-m)-2.5,0]
+        spring=always_redraw(lambda : ParametricFunction(zigzag,t_range=[0,cube.get_critical_point(LEFT)[0]+6])) 
+        self.add(wall)
+        self.add(floor)
+        self.add(spring)
         cube.add_updater(move)
         self.add(cube)
         self.wait()
