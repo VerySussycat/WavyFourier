7afc1ea657fb0321478dda6f161399e709adcbd0
VerySussycat <108729015+VerySussycat@users.noreply.github.com>
2025-05-26 13:48:30 +0530

    Update SHM.py

--- SHM.py
+++ SHM.py
@@ -1,7 +1,7 @@
 from manim import * #Import everything from manim
 import numpy as np
 vel=0
-class Blockonspring(Scene):#Kuch hai
+class Blockonspring(ThreeDScene):#Kuch hai
     def construct(self):
         x0=0    #Equilibrium position of block(unstretched string)
         m=1     #mass of block
@@ -9,9 +9,9 @@ class Blockonspring(Scene):#Kuch hai
         A=2     #Amplitude(initial displacement from equilibrium in this case as v_init=0)
         phi=0   #phase constant
         omega=(k/m)**0.5
-        cube=Square(color=DARK_BLUE,fill_opacity=0.5).move_to([-3,-2,0],aligned_edge=LEFT).scale(0.5) #initialize block
-        wall=Line([-6,-2.5,0],[-6,4,0])
-        floor=Line([-6,-2.5,0],[6,-2.5,0])
+        cube=Cube(color=DARK_BLUE,fill_opacity=0.5).move_to([-3,-2,0],aligned_edge=LEFT).scale(0.5) #initialize block
+        wall=Surface(lambda u,v:[-6,u,v],u_range=[-2.5,2.5],v_range=[-5,5],checkerboard_colors=False,fill_color=GREEN,stroke_opacity=0,stroke_color=GREEN)
+        floor=Surface(lambda u,v:[u,-2.5,v],u_range=[-6,4],v_range=[-5,5],checkerboard_colors=False,fill_color=GREEN,stroke_opacity=0,stroke_color=GREEN)
         t=ValueTracker(0)
         def move(mob,dt):
             global vel
@@ -20,13 +20,20 @@ class Blockonspring(Scene):#Kuch hai
             vel+=acc*dt
             mob.shift(vel*dt*RIGHT)
         def zigzag(t):
-            l=np.modf(30*(t/(cube.get_critical_point(LEFT)[0]+6)))[1]+1
-            m=np.modf(30*(t/(cube.get_critical_point(LEFT)[0]+6)))[0]
+            l=np.modf(31*(t/(cube.get_critical_point(LEFT)[0]+6)))[1]+1
+            m=np.modf(31*(t/(cube.get_critical_point(LEFT)[0]+6)))[0]
             if l%2==0:
-                return [t-6,0.4*m-2.5,0]
+                return [t-6,0.7*m-2,0]
             else:
-                return [t-6,0.4*(1-m)-2.5,0]
-        spring=always_redraw(lambda : ParametricFunction(zigzag,t_range=[0,cube.get_critical_point(LEFT)[0]+6])) 
+                return [t-6,0.7*(1-m)-2,0]
+        def spiral(t):
+            l=cube.get_critical_point(LEFT)[0]+6
+            n=50
+            r=0.5
+            w=2*PI*r*n
+            v=l
+            return [v*t-6,r*np.sin(w*t)-2,r*np.cos(w*t)]
+        spring=always_redraw(lambda : ParametricFunction(spiral,t_range=[0,1])) 
         self.add(wall)
         self.add(floor)
         self.add(spring)
@@ -34,10 +41,3 @@ class Blockonspring(Scene):#Kuch hai
         self.add(cube)
         self.wait()
         self.play(t.animate.set_value(15),run_time=15)
-        
-            
-
-
-
-        
-        
